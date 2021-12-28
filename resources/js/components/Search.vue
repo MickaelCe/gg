8df491d8043eb1632896b1f68a0149a17efcc0ca
mickael <mickael.cecen@gmail.com>
2021-12-28 19:58:40 +0100

    live search now with infinite scroll

--- resources/js/components/Search.vue
+++ resources/js/components/Search.vue
@@ -2,17 +2,16 @@
     <div>
         <input type="text" v-model='q' class="form-control inputsearch" placeholder="Rechercher un jeux...">
         <div v-if="q != ''">
-            <div v-for="game in getFilteredGames" v-bind:key="game.names">
+            <div v-for="game in getFilteredGames" v-bind:key="game.id">
                 <div class="card my-3 bg-transparent">
                     <a href="#"><img :src="`${game.img_url}`" class="imgsearch" alt="Couverture du jeux" /></a>
                 </div>
             </div>
+            <div v-observe-visibility="handleScrolledToBottom"></div>
         </div>
             <div v-else>
-                <h2>Faites une recherche</h2>
+                <h2 class="text-white">Faites une recherche</h2>
             </div>
-
-        <div v-observe-visibility="handleScrolledToBottom"></div>
     </div>
 </template>
 
@@ -23,16 +22,26 @@
             return {
                 games: [],
                 q: '',
-                page: 1
+                page: 5
             }
         },
 
         methods: {
+            // fetchGames() {
+            //     axios.get(`/api/games?page=${this.page}`)
+            //     .then (response => {
+            //         this.games.push(...response.data.data);
+            //         console.log(this.page);
+            //     })
+            //     .catch (error => {
+            //         console.log(error)
+            //     })
+            // },
             fetchGames() {
-                axios.get(`/api/games?page=${this.page}`)
+                axios.get(`/api/games`)
                 .then (response => {
-                    console.log(response.data.data);
-                    this.games.push(...response.data.data);
+                    console.log(response.data);
+                    this.games = response.data;
                 })
                 .catch (error => {
                     console.log(error)
@@ -40,23 +49,20 @@
             },
                 handleScrolledToBottom (isVisible){
                     if (!isVisible) { return }
-                    this.page++
+                    this.page+=10
                     this.fetchGames()
-                }   
+                },
         },
-
-        
 
         computed: {
             getFilteredGames(){
                 return this.games.filter(games => {
-                    return games.names.toLowerCase().includes(this.q.toLowerCase());
-                })
-            }
+                    return games.names.toLowerCase().includes(this.q.toLowerCase())
+                }).slice(0,this.page);
+            },
         },
-
-        mounted() {
-            this.fetchGames();
-        }
+            mounted() {
+                this.fetchGames();
+            }
     }
 </script>
