680c7c77cd277b8f667fe6c803906fdb50ed7904
mickael <mickael.cecen@gmail.com>
2022-01-06 17:09:51 +0100

    adding img

---
+++ resources/js/components/Carousel.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="carousel">
+      <slot :currentSlide="currentSlide"/>
+      <div class="pagination">
+          <div v-for="(slide, index) in getSlideCount" :key="index" :class="{ active : index + 1 === currentSlide }">
+              <img @click="goToSlide(index)" v-if="slide === 1" :src="require(`../assets/dl4d.png`).default" class="paginationImg">
+              <img @click="goToSlide(index)" v-else-if="slide === 2" :src="require(`../assets/dark.png`).default" class="paginationImg">
+              <img @click="goToSlide(index)" v-else-if="slide === 3" :src="require(`../assets/dnier.png`).default" class="paginationImg">
+              <img @click="goToSlide(index)" v-else-if="slide === 4" :src="require(`../assets/dre.png`).default" class="paginationImg">
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import { ref, reactive , onMounted} from '@vue/composition-api'
+export default {
+    setup() {
+        const currentSlide = ref(1);
+        const getSlideCount = ref(null);
+
+        const goToSlide = (index) =>{
+            currentSlide.value = index + 1;
+        }
+
+        onMounted(() =>{
+            getSlideCount.value = document.querySelectorAll('.slide').length;
+        });
+        return { currentSlide, getSlideCount, goToSlide};
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="home">
+    <Carousel class="carousel" v-slot="{ currentSlide }">
+        <Slide v-for="(slide, index) in carouselSlides" v-bind:key="index">
+                <img v-show="currentSlide === index + 1" :src="require(`../assets/${slide}.png`).default" alt="">
+        </Slide>
+    </Carousel>
+</div>   
+</template>
+
+<script>
+
+import Carousel from "./Carousel.vue";
+import Slide from "./Slide.vue";
+
+export default {
+        name: "Home",
+        components: { Carousel, Slide },
+        data(){
+            return{
+                carouselSlides: ["dl4d","dark","dnier","dre"],
+            };
+        },
+};
+
+
+</script>
+
+<style>
+
+</style>
