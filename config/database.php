0563f9c860310d343306c3095dfdb10d573a14f6
mickael <mickael.cecen@gmail.com>
2022-02-14 14:58:53 +0100

    test

--- config/database.php
+++ config/database.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL=parse_url(‘DATABASE_URL’);
 
 return [
 
@@ -18,8 +17,6 @@ return [
     */
 
     'default' => env('DB_CONNECTION', 'mysql'),
-
-    
 
     /*
     |--------------------------------------------------------------------------
@@ -50,11 +47,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => $DATABASE_URL['host'],
-            'port' => $DATABASE_URL['port'],
-            'database' => ltrim($DATABASE_URL['path'],'/'),
-            'username' => $DATABASE_URL['username'],
-            'password' => $DATABASE_URL['password'],
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
