f041ed794877b83038181e6dab7184976a673725
mickael <mickael.cecen@gmail.com>
2022-02-16 16:35:07 +0100

    update mobile login screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
 
     <!-- Main content -->
     <div class="content pt-3 mobilehome">
-                <a href="/show/767" id="l4d" class="bgHomeImg image-container"><img id="l4d" src="{{ asset('images/l4d.png') }}"><div class="mask d-flex justify-content-end align-items-end flex-column action h4 pr-3 pb-3">Title 1 <br><small class="text-white">Some text</small></div></a>
-                <a href="/show/54" id="ARK"  class="bgHomeImg image-container"><img id="ARK" src="{{ asset('images/ARK.png') }}"><div class="mask d-flex justify-content-end align-items-end flex-column action h4 pr-3 pb-3">Title 2 <br><small class="text-white">Some text</small></div></a>
-                <a href="/show/235" id="re" class="bgHomeImg image-container"><img id="re" src="{{ asset('images/REM.png') }}"><div class="mask d-flex justify-content-end align-items-end flex-column action h4 pr-3 pb-3">Title 3 <br><small class="text-white">Some text</small></div></a>
-                <a href="/show/465" id="nier" class="bgHomeImg image-container"><img id="nier" src="{{ asset('images/nier.png') }}"><div class="mask d-flex justify-content-end align-items-end flex-column action h4 pr-3 pb-3">Title 4 <br><small class="text-white">Some text</small></div></a>
-                <a href="/show/2412" id="b4b" class="bgHomeImg image-container"><img id="b4b" src="{{ asset('images/b4b.jpg') }}"><div class="mask d-flex justify-content-end align-items-end flex-column action h4 pr-3 pb-3">Title 5 <br><small class="text-white">Some text</small></div></a>
+                <a href="/show/767" id="l4d" class="bgHomeImg image-container"><img id="l4d" src="{{ asset('images/l4d.png') }}"><div class="mask d-flex justify-content-end align-items-end flex-column action h4 pr-3 pb-3">Left 4 Dead <br><small class="text-white">Explore</small></div></a>
+                <a href="/show/54" id="ARK"  class="bgHomeImg image-container"><img id="ARK" src="{{ asset('images/ARK.png') }}"><div class="mask d-flex justify-content-end align-items-end flex-column action h4 pr-3 pb-3">Ark <br><small class="text-white">Explore</small></div></a>
+                <a href="/show/235" id="re" class="bgHomeImg image-container"><img id="re" src="{{ asset('images/REM.png') }}"><div class="mask d-flex justify-content-end align-items-end flex-column action h4 pr-3 pb-3">Resident evil 2 <br><small class="text-white">Explore</small></div></a>
+                <a href="/show/465" id="nier" class="bgHomeImg image-container"><img id="nier" src="{{ asset('images/nier.png') }}"><div class="mask d-flex justify-content-end align-items-end flex-column action h4 pr-3 pb-3">Nier automata <br><small class="text-white">Explore</small></div></a>
+                <a href="/show/2412" id="b4b" class="bgHomeImg image-container"><img id="b4b" src="{{ asset('images/b4b.jpg') }}"><div class="mask d-flex justify-content-end align-items-end flex-column action h4 pr-3 pb-3">Metal gear rising <br><small class="text-white">Explore</small></div></a>
     </div>
 
     <div class="content pt-3 desktophome">
